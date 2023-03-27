f1745cc54c9eaaeafe35246f16d6780881125a26
GerardoLT <120034634@upq.edu.mx>
2023-03-27 15:21:43 -0600

    Update AND.py

--- AND.py
+++ AND.py
@@ -1,31 +1,20 @@
-import random 
-import itertools
+import numpy as np
 
-def AND(self,num_vars):
-    input_values = [[0, 1] for _ in range(num_vars)]
-    input_combinations = list(itertools.product(*input_values))
+def linear_threshold_gate(dot: int, T: float) -> int:
+    if dot >= T:
+        return 1
+    else:
+        return 0
+    
+def funcionAND(umbral):
+    for i in range(0,4):
+        activation = linear_threshold_gate(dot_products[i], umbral)
+        print(f'Activation: {activation}')
+    print(' ')   
+input_table = np.array([[0,0],[0,1],[1,0],[1,1]])
+pesos = np.array([9,2])
+dot_products = input_table @ pesos
+umbral = 9
+print(f'Dot products: {dot_products}')
+funcionAND(umbral)
 
-    and_results = []
-    for inputs in input_combinations:
-        result = all(inputs)
-        and_results.append(result)
-
-    table = list(zip(input_combinations, and_results))
-   
-    for row in table:
-        input_str = " ".join([str(input_val) for input_val in row[0]])
-        result_str = str(row[1])
-        print(f"{input_str} = {result_str}")
-
-
-def TAND(self,num_trials, num_vars):
-    for i in range(num_trials):
-        inputs = [random.randint(0, 2) for _ in range(num_vars)]
-        for row in table:
-         nuevos_val = " ".join([str(self.input_val) for input_val in row[0]])
-         
-        output = all(inputs)
-        if output:
-            print(f"Inputs: {inputs}; Output: {output}")
-        else:
-            print(f"Inputs: {inputs}; Output: {output}")
